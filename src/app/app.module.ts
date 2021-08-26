3020f4fdf346223f2d8cd80939c4c4f3d1dceffb
santy <santiago.becerra@uptc.edu.co>
2021-08-26 09:39:37 -0500

    page ed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageEdUptcComponent } from './page-ed-uptc/page-ed-uptc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageEdUptcComponent
   ],
   imports: [
     BrowserModule,
