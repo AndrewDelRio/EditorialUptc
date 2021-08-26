5090d1d08d654cf734d155628e4ea5033f077e55
santy <santiago.becerra@uptc.edu.co>
2021-08-26 09:42:02 -0500

    page ed

---
+++ src/app/page-ed-uptc/page-ed-uptc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-ed-uptc',
+  templateUrl: './page-ed-uptc.component.html',
+  styleUrls: ['./page-ed-uptc.component.scss']
+})
+export class PageEdUptcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
