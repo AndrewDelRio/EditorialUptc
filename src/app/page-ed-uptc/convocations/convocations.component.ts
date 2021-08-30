a5622ee69de08e535fd204bfdbd5c7de967100ba
Richardagudelo <richard.agudelo@uptc.edu.co>
2021-08-30 15:24:35 -0500

    Creacion del area de navegacion rapida o navbar
    -se creo el componente de navegation-page
    -se agrego el navbar con las pestañas Inicio, libros, novedades y convicatorias
    -se crearon los componentes de libros, novedades y convicatorias y se enlazaron en el navbar
    -se modificaron las router-outlet

---
+++ src/app/page-ed-uptc/convocations/convocations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-convocations',
+  templateUrl: './convocations.component.html',
+  styleUrls: ['./convocations.component.scss']
+})
+export class ConvocationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
