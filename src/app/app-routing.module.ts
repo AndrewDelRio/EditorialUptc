09e89bdfbc5c111f7b76fc68bb742e63df732bf3
santy <santiago.becerra@uptc.edu.co>
2021-08-31 17:11:07 -0500

    carousel v5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-import{CarouselUptcComponent } from '../app/page-ed-uptc/carousel-uptc/carousel-uptc.component';
-import { PageEdUptcComponent } from './page-ed-uptc/page-ed-uptc.component';
-const routes: Routes = [
-  {
-    path : '',
-    redirectTo : '/home',
-    pathMatch: 'full'
-  },
-  {
-    path : 'home',
-    component : PageEdUptcComponent
-  },
-  {
-    path : 'home/banner',
-    component : CarouselUptcComponent
-=======
 import { BooksComponent } from './page-ed-uptc/books/books.component';
 import { CarouselUptcComponent } from './page-ed-uptc/carousel-uptc/carousel-uptc.component';
 import { ConvocationsComponent } from './page-ed-uptc/convocations/convocations.component';
@@ -45,7 +28,6 @@ const routes: Routes = [
         component: NoveltiesComponent
       }
     ]
->>>>>>> origin/develop
   }
 ];
 
