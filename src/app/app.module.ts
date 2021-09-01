28236ffae3389811e17b98a781e392aa2153452d
santy <santiago.becerra@uptc.edu.co>
2021-09-01 17:21:43 -0500

    improve-interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,10 @@ import { AppComponent } from './app.component';
 import { PageEdUptcComponent } from './page-ed-uptc/page-ed-uptc.component';
 import { NavBarEncabezado1Component } from './page-ed-uptc/nav-bar-encabezado1/nav-bar-encabezado1.component';
 import { CarouselUptcComponent } from './page-ed-uptc/carousel-uptc/carousel-uptc.component';
-<<<<<<< HEAD
-import { EventNewsComponent } from './page-ed-uptc/event-news/event-news.component';
-=======
 import { NavegationPageComponent } from './page-ed-uptc/navegation-page/navegation-page.component';
 import { BooksComponent } from './page-ed-uptc/books/books.component';
 import { NoveltiesComponent } from './page-ed-uptc/novelties/novelties.component';
 import { ConvocationsComponent } from './page-ed-uptc/convocations/convocations.component';
->>>>>>> origin/develop
 
 @NgModule({
   declarations: [
@@ -21,14 +17,10 @@ import { ConvocationsComponent } from './page-ed-uptc/convocations/convocations.
     PageEdUptcComponent,
     NavBarEncabezado1Component,
     CarouselUptcComponent,
-<<<<<<< HEAD
-    EventNewsComponent
-=======
     NavegationPageComponent,
     BooksComponent,
     NoveltiesComponent,
     ConvocationsComponent
->>>>>>> origin/develop
   ],
   imports: [
     BrowserModule,
